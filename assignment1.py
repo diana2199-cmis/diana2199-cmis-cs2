631b4d491eb0229182816ef1005d19a422881026
cmis <cmis@cmis-hs12.(none)>
2016-02-03 08:23:49 +0700

    Added changes to assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,16 +1,40 @@
+# my name
 Name = "Diana"
+# my age
 Age = 15.8
+# my height in meters
 Height = 1.6
+# length of a square
 LofSquare = 5
+# lenght of a rectangle
 LofRectangle = 10
+# height of a rectangle
 HofRectangle = 5
+# my age in month
 AgeinMonth = Age * 12
+# average female death age
 AverageDeathAge = 71
+# remainding years of my life
 YearsLeft = AverageDeathAge - Age
+# how many feet in a meter
 MinFt = 3.28084
+# my height in feet
 HeightinFt = Height * MinFt
+# average height of 15 years old Korean girls
 AverageHeight = 160.5
+# height difference of my height and average height
 HeightDifference = Height - AverageHeight
+# area of the square
 AreaofSquare = LofSquare ** 2
+# volume of cube
 VolumeofCube = LofSquare ** 3 / 2
+# one ninth of area of rectangle
 OnenofRectangleA = LofRectangle * HofRectangle * (1/9.0)
+
+print "Hello. My name is " + str(Name) + ". My age is " + str(Age) + ", which is about " + str(AgeinMonth) + " in month. I can live about " + str(YearsLeft) + " more years. My height is about " + str(Height) + " meters."
+
+print "Hello. My name is", Name,". My age is", Age, "which is about",  AgeinMonth, ". I can live about", YearsLeft, "more years. My height is about", Height, "meters."
+
+SmileyFace = ";)"
+print SmileyFace * 10000
+

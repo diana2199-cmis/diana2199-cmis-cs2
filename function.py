0a1ecd4cf16d75e40be8c415ca21398de9fef704
cmis <cmis@cmis-hs12.(none)>
2016-02-10 10:50:32 +0700

    Added changes to function.py

--- function.py
+++ function.py
@@ -1,4 +1,22 @@
+#addition operation
 def add(a,b):
-	return add(a,b)
-
-print add
+	return a+b
+w= add(3,4)
+print w
+#subtraction operation
+def sub(a,b):
+	return a-b
+x= sub(5,3)
+print x
+#multiplication operation
+def mul(a,b):
+	return a*b
+y= mul(4,4)
+print y
+#division operation
+def div(a,b):
+	return a/b
+z= div(2.0,3)
+print z
+#takes number of seconds and turn into number of hours
+def 

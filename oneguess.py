b840df51c8e47037e31c292d5747637846f09e6c
cmis <cmis@cmis-hs12.(none)>
2016-03-14 10:55:32 +0700

    Added changes to oneguess.py

---
+++ oneguess.py
@@ -0,0 +1,4 @@
+minimumNumber = raw_input("What is the minimum number? ")
+maximumNumber = raw_input("What is the maximum number? ")
+
+print I'm thinking of a number from minimumNumber and maximumNumber

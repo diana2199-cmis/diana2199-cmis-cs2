4dbb58cb07c87e28cf092e5cc67205183de1edd5
cmis <cmis@cmis-hs12.(none)>
2016-03-14 10:41:06 +0700

    Added changes to simpleprogram.py

---
+++ simpleprogram.py
@@ -0,0 +1,38 @@
+import math
+
+print "Start"
+
+birthMonth = raw_input("Type your birth month (in number) = ")
+
+#numbers used for calculation
+firstNumber = 5
+secondNumber = 6
+thirdNumber = 4
+fourthNumber = 9
+fifthNumber = 5
+sixthNumber = 163
+
+#the calculation
+firstCalc = int(birthMonth)*int(firstNumber)
+secondCalc = int(firstCalc)+int(secondNumber)
+thirdCalc = int(secondCalc)*int(thirdNumber)
+fourthCalc = int(thirdCalc)+int(fourthNumber)
+fifthCalc = int(fourthCalc)*int(fifthNumber)
+print fifthCalc
+
+#printing and more calculation
+print "Add the number above by your birth date."
+print "Then subtract 165 from the result."
+print "This number is your birth month and date."
+print "ex) 525 is May 25th, 609 is June 9th."
+
+work = raw_input("Did it work? (Yes or No)")
+Yes = True
+No = False
+if True 
+print Yay! :)
+else 
+print Try again!
+
+print "End"
+

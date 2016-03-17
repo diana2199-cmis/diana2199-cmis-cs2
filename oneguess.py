671e16ca93c6334f4a8efe6992ee4fd3fcc1da82
cmis <cmis@cmis-hs02.(none)>
2016-03-17 08:37:14 +0700

    Added changes to oneguess.py

--- oneguess.py
+++ oneguess.py
@@ -1,4 +1,22 @@
-minimumNumber = raw_input("What is the minimum number? ")
-maximumNumber = raw_input("What is the maximum number? ")
+import random
 
-print I'm thinking of a number from minimumNumber and maximumNumber
+def rangeNumber(minimumNumber,maximumNumber):
+    return random.radint(minimumNumber,maximumNumber)
+
+def inputGuess():
+    random = random.random
+    if guess == str(random):
+        yourGuess = True
+    if guess > str(random):
+        yourGuess = "That's over by " + abs(random - guess)
+    if guess < str(random):
+        yourGuess = "That's under by " + abs(random - guess)
+
+
+
+
+def main():
+
+    minimumNumber = raw_input("What is the minimum number? ")
+    maximumNumber = raw_input("What is the maximum number? ")
+    yourGuess = raw_input("What do you thik it is?: ")

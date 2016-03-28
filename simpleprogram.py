0cc0d08d58df14874d2b64af49cfd6631c3945c8
cmis <cmis@cmis-hs02.(none)>
2016-03-28 10:13:57 +0700

    Added changes to simpleprogram.py

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,4 +1,5 @@
 import math
+
 #this simple program calculates your lucky number using you birthmonth and birthdate
 
 #this function does the calculation
@@ -6,7 +7,7 @@ def calculation(birthmonth, birthdate):
 	return (birthmonth*5-5+birthdate)/6+2
 
 def output(name,birthmonth, birthdate,result):
-    return """{}, Your birthmonth is {}, your birthdate is {}, your lucky number is {}""".format(name,birthmonth,birthdate,result)
+    return """Hi {}, Your birthmonth is {}, your birthdate is {}, your lucky number is {}""".format(name,birthmonth,birthdate,result)
 
 #this main function organizes functions above and calls them in correct order
 def main():

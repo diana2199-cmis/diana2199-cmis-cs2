5224b2054188765e622cc1beedf69f689430146c
cmis <cmis@cmis-hs02.(none)>
2016-03-29 13:37:36 +0700

    Added changes to oneguess.py

--- oneguess.py
+++ oneguess.py
@@ -1,22 +1,32 @@
 import random
 
-def rangeNumber(minimumNumber,maximumNumber):
-    return random.radint(minimumNumber,maximumNumber)
-
-def inputGuess():
-    random = random.random
-    if guess == str(random):
-        yourGuess = True
-    if guess > str(random):
-        yourGuess = "That's over by " + abs(random - guess)
-    if guess < str(random):
-        yourGuess = "That's under by " + abs(random - guess)
-
-
-
+def output(target,guess,difference):
+    if guess == target:
+        print """
+The target was {}.
+Your guess was {}.
+That is correct! You must be a phychic!
+""".format(target,guess)
+    if guess > target:
+        print """
+The target was {}.
+Your guess was {}.
+That's over by {}.
+""".format(target,guess,difference)
+    if guess < target:
+        print """
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""".format(target,guess,difference)
 
 def main():
+    minimumNumber = int(raw_input ("What is the minimum number? "))
+    maximumNumber = int(raw_input ("What is the maximum number? "))
+    print "I'm thinking of a number from", minimumNumber, "and", maximumNumber,"."
+    guess = int(raw_input ("What do you think it is?: "))
+    target = random.randint(int(minimumNumber),int(maximumNumber))
+    difference = abs(guess-target)
+    out= output(target,guess,difference)
 
-    minimumNumber = raw_input("What is the minimum number? ")
-    maximumNumber = raw_input("What is the maximum number? ")
-    yourGuess = raw_input("What do you thik it is?: ")
+main()

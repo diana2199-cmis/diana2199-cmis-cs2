b16757efe5e008675916d701fb6c6ca5faaa060a
cmis <cmis@cmis-hs02.(none)>
2016-04-01 10:55:27 +0700

    Added changes to conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,18 @@
+# This program makes a story based on your input about youself
+# It asks for...
+# 1. First letter of your first and last name 
+# 2. Age, grade, and number of family members
+# 3. Birthday, month, and year
+# 4. 
+
+import random
+import math
+
+def output(name):
+    return """ Hello {}, """.format(name)
+
+def main(): 
+    name = str(raw_input("What is your name? "
+    print output
+
+main()

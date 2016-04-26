e792ee1b4a75d445f2b4749008ec7f872247273a
cmis <cmis@cmis-hs02.(none)>
2016-04-26 08:33:32 +0700

    Added changes to cs2quiz2.py

--- cs2quiz2.py
+++ cs2quiz2.py
@@ -1,39 +1,38 @@
 #PART 1: Terminology
 #1) Give 3 examples of boolean expressions.
-#a) True, False
-#b) and, or
-#c) ==, >, <
+#a) True, False #point
+#b) and, or #point
+#c) ==, >, < #point
 #
 #2) What does 'return' do?
-# The word return takes in an argument from a function and returns a result. The result is called the return value.
+# The word return takes in an argument from a function and returns a result. The result is called the return value. #print 
 #
 #3) What are 2 ways indentation is important in python code?
-#a) Indentation tells where function definition ends
-#b) It tells when to call a function and return a value
-#
+#a) Indentation tells where function definition ends #point 
+#b) It tells when to call a function and return a value #point
 
 #PART 2: Reading
 #Type the values for 12 of the 16 of the variables below.
 #
-#problem1_a) 36
-#problem1_b) sqrt of 3
-#problem1_c) 0
-#problem1_d) 5
+#problem1_a) 36 
+#problem1_b) sqrt of 3 #point
+#problem1_c) 0 #point
+#problem1_d) 5 #point
 #
-#problem2_a) True
-#problem2_b) False
-#problem2_c) False
+#problem2_a) True #point
+#problem2_b) False #point
+#problem2_c) False #point
 #problem2_d) False
 #
-#problem3_a) 0.3
-#problem3_b) 0.5
-#problem3_c) 1
-#problem3_d) 0.5
+#problem3_a) 0.3 #point
+#problem3_b) 0.5 #point
+#problem3_c) 1 
+#problem3_d) 0.5 #point
 #
-#problem4_a) 7
+#problem4_a) 24
 #problem4_b) 5
 #problem4_c) 0.125
-#problem4_d) 5
+#problem4_d) 5 #point
 #
 
 #PART 3: Programming
@@ -43,7 +42,8 @@
 #If they don't, it should print a message telling them they didn't follow 
 #the directions.
 #Be sure to use the program structure you've learned (main function, processing function, output function)
-
+#
+#points for 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
 import math
 
 def compare(a,b,c):

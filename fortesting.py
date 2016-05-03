a9b6fea1ed330bd0f497ea336f8c4e995c92112d
cmis <cmis@cmis-hs02.(none)>
2016-05-03 10:50:16 +0700

    Added changes to fortesting.py

--- fortesting.py
+++ fortesting.py
@@ -1,13 +1,2 @@
-#adder
-def adder(next,total):
-    if next == "":
-        print "The sum is: " + str(total)
-    else:
-        total += float(next)
-        next = raw_input("Running total: " + str(total) + "\n" + "Next Number: ")
-        return adder(next,total)
-
-def main():
-    adder(0,0)
-
-main()
+#biggest
+def 

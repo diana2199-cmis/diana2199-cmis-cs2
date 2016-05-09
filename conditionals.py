74b2876cef43009f4ca17d76b37f7da66f4a91f9
cmis <cmis@cmis-hs02.(none)>
2016-05-09 08:41:27 +0700

    Added changes to conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,4 +1,4 @@
-# This program makes a story based on your input about youself
+# This program makes a story based on your input about yourself
 # It asks for...
 # 1. First letter of your first and last name 
 # 2. Age, grade, and number of family members
@@ -8,11 +8,23 @@
 import random
 import math
 
+def one(firstLetter):
+    if firstLetter == "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m":
+        return "She lived with her family in a city. "
+    elif #continue this part:
+        return "She lived alone in a forest. "
+
 def output(name):
-    return """ Hello {}, """.format(name)
+    return """*Hello {} :)""".format(name)
 
-def main(): 
-    name = str(raw_input("What is your name? "
-    print output
-
+def main():
+    name = raw_input("*What is your name? ")
+    print output(name)
+    print "*This program will make a story based on your input about yourself."
+    print ""
+    print "<Once upon a time, there was a girl named Iva.>"
+    print ""
+    firstLetter = raw_input("*What is the first letter of you name? ")
+    print one(firstLetter)
+    
 main()

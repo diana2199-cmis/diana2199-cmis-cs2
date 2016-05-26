85906636acf30a57cc5b0e42815a14173f953568
cmis <cmis@cmis-hs02.(none)>
2016-05-26 08:43:09 +0700

    Added changes to fortesting.py

--- fortesting.py
+++ fortesting.py
@@ -4,16 +4,31 @@
 #while False:
 #    print ":("
 
+#x = 0
+#while x <10:
+#	print x
+#        x += 1
+
 #x = 100
 #while x >= 0:
 #    print x
 #    x -= 1
 
-def countup(x):
-    while x >= 0:
-         x -= 1
+#def countFrom1(x,y):
+#    while x < y:
+#        print x
+#        x += 1
+#    while x >= y:
+#        print x
+#        x -= 1
+#countFrom1(2,5)
 
-def main():
-    countup(100)
+def addOdds(n):
+    if n > 0:
+        while n > 0:
 
-    main()
+    
+    elif n > 0:
+        while n % 2 == 1:
+            n+=n
+addOdds(4)

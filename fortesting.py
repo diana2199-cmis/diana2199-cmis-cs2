1bf42ae8dcf5f349196ccc90fa5e0cc7a67a260d
cmis <cmis@cmis-hs02.(none)>
2016-05-27 10:56:50 +0700

    Added changes to fortesting.py

--- fortesting.py
+++ fortesting.py
@@ -23,12 +23,38 @@
 #        x -= 1
 #countFrom1(2,5)
 
-def addOdds(n):
-    if n > 0:
-        while n > 0:
+#def addOdds(n):
+#    sum = 0
+#    if n > 0:
+#        while n > 0:
+#            if n % 2 == 1:
+#                sum += n
+#            n -= 1
+#    elif n < 0:
+#        while n < 0:
+#            if n % 2 == 1:
+#                sum += n
+#            n += 1
+#    return sum
+#print addOdds(9)
 
-    
-    elif n > 0:
-        while n % 2 == 1:
-            n+=n
-addOdds(4)
+def grid(w,h):
+    out = ""
+    while w > 0:
+        print ".",
+        w -= 1
+        while h > 0:
+            print h * w
+            h -= 1
+print grid(4,5)
+
+def grud(w,h):
+    out = ""
+    y = 0
+    while y < h:
+        x = 0
+        while x < w:
+            out += "."
+            x += x
+        y += 1
+        out += "
